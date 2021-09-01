2f422d26c77767f70eabad5f65344952900585f9
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2021-09-01 21:59:38 +1000

    refactor: Apparently you only need to track lowest price and profit, DOH!

--- 2021/InterviewCake/StockPrice.swift
+++ 2021/InterviewCake/StockPrice.swift
@@ -17,57 +17,29 @@
  yesterday.
  */
 
-func getMaxProfit(from prices: [Int]) -> Int {
-    guard !prices.isEmpty else {
-        return 0
-    }
-    print("prices isn't empty")
-    guard prices.count > 2 else {
-        return 0
-    }
-    print("prices isn't empty")
-    guard !(prices.count == 2) else {
-        return prices[1] - prices[0]
-    }
+let stockPrices = [10, 7, 5, 8, 11, 9]
+
+func getMaxProfit(from prices: [Int]) -> Int? {
     // more than 2
+    guard prices.count >= 2 else {
+        return nil
+    }
 
-    var prices = prices
-    var currentPrice = prices.removeFirst()
-    var currentMaxProfit = prices[0] - currentPrice
-    // prices now has mutated to be the rest of the prices.
+    var currentMinPrice = prices[0]
+    var currentMaxProfit = prices[1] - currentMinPrice
 
-    // we need at least two
-    while prices.count > 1 {
-        print("currentPrice:", currentPrice)
-        print("the rest of the prices:", prices)
-
-        // this is O(n^2) ?
-        // two O(n) nested (the max is an O(n) operation)
-
-        guard let maxPrice = prices.max() else {
-            return currentMaxProfit
-        }
-        print("maxPrice from the rest:", maxPrice)
-
-        let profit = maxPrice - currentPrice
-        print("profit:", profit)
-
-        print("currentMaxProfit:", currentMaxProfit)
+    for currentPrice in prices[1...] {
+        let profit = currentPrice - currentMinPrice
 
         currentMaxProfit = max(currentMaxProfit, profit)
 
-        print("currentMaxProfit after adjustment:", currentMaxProfit)
-        // to seed the next round
-
-        currentPrice = prices.removeFirst()
+        currentMinPrice = min(currentMinPrice, currentPrice)
     }
 
     return currentMaxProfit
 }
 
-let stockPrices = [10, 7, 5, 8, 11, 9]
-
-print(getMaxProfit(from: stockPrices))
+print(getMaxProfit(from: stockPrices)!)
 
 
-print(getMaxProfit(from: [10, 6, 5, 4]))
+print(getMaxProfit(from: [10, 6, 5, 4])!)
