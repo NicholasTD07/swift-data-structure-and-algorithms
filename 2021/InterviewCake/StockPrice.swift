0ab09329f5fd0ade324ffa3e099e087c4180c601
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2021-09-01 21:55:03 +1000

    fix: So we get negative profit if the prices keep going down

--- 2021/InterviewCake/StockPrice.swift
+++ 2021/InterviewCake/StockPrice.swift
@@ -1,4 +1,21 @@
-let stockPrices = [10, 7, 5, 8, 11, 9]
+// https://www.interviewcake.com/question/swift/stock-price
+
+/*
+ First, I wanna know how much money I could have made yesterday if I'd been
+ trading Apple stocks all day.
+
+ So I grabbed Apple's stock prices from yesterday and put them in an array
+ called stockPrices, where:
+
+ The indices are the time (in minutes) past trade opening time, which was 9:30am
+ local time. The values are the price (in US dollars) of one share of Apple
+ stock at that time. So if the stock cost $500 at 10:30am, that means
+ stockPrices[60] = 500.
+
+ Write an efficient function that takes stockPrices and returns the best profit
+ I could have made from one purchase and one sale of one share of Apple stock
+ yesterday.
+ */
 
 func getMaxProfit(from prices: [Int]) -> Int {
     guard !prices.isEmpty else {
@@ -16,7 +33,7 @@ func getMaxProfit(from prices: [Int]) -> Int {
 
     var prices = prices
     var currentPrice = prices.removeFirst()
-    var currentMaxProfit = 0
+    var currentMaxProfit = prices[0] - currentPrice
     // prices now has mutated to be the rest of the prices.
 
     // we need at least two
@@ -48,4 +65,9 @@ func getMaxProfit(from prices: [Int]) -> Int {
     return currentMaxProfit
 }
 
+let stockPrices = [10, 7, 5, 8, 11, 9]
+
 print(getMaxProfit(from: stockPrices))
+
+
+print(getMaxProfit(from: [10, 6, 5, 4]))
